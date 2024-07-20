85296824f3b4c0f9489e7fdaf437078398eb4964
limsubinn <mymyx02@naver.com>
2024-07-20 10:00:16 +0900

    BOJ 22251: 빌런 호석

--- limsubin/baekjoon/boj22251.py
+++ limsubin/baekjoon/boj22251.py
@@ -1,17 +1,11 @@
 # BOJ 22251: 빌런 호석
 # https://www.acmicpc.net/problem/22251
 
-def is_changeable(x, y, p):
+def count_diff(x, y):
     cnt = 0
-    for i in range(k):
-        for j in range(7):
-            if x[i][j] != y[i][j]:
-                cnt += 1
-                if cnt > p:
-                    return False
-    if cnt < 1:
-        return False
-    return True
+    for i in range(7):
+        cnt += abs(x[i] - y[i])
+    return cnt
 
 led = [[1, 1, 1 ,1, 0, 1, 1],
        [0, 0, 1, 0, 0, 1, 0],
@@ -26,18 +20,28 @@ led = [[1, 1, 1 ,1, 0, 1, 1],
 
 n, k, p, x = map(int, input().split())
 
+diff = [[0] * 10 for _ in range(10)]
+# i -> j 변환 횟수
+for i in range(10):
+    for j in range(10):
+        diff[i][j] += count_diff(led[i], led[j])
+
 answer = 0
-led_x = [led[int(i)] for i in str(x).zfill(k)] # 초기 LED 상태
+xx = list(map(int, str(x).zfill(k))) # 초기 LED 상태
 
 for y in range(1, n+1):
     # 같은 수 지나감!
     if x == y:
         continue
 
-    led_y = [led[int(i)] for i in str(y).zfill(k)] # 바꿀 LED 상태
+    yy = list(map(int, str(y).zfill(k))) # 바꿀 LED 상태
 
-    # 두 수의 차이가 1 이상 p 이하일 경우!
-    if is_changeable(led_x, led_y, p):
+    cnt = 0
+    for i in range(k):
+        cnt += diff[xx[i]][yy[i]]
+        if cnt > p:
+            break
+    else:
         answer += 1
 
 print(answer)
